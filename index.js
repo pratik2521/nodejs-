ab48dcce6f89204fad1b40bef4ba91b519b094db
sumityadav <sumitinbox100@gmail.com>
2024-04-28 00:04:56 +0530

    add

---
+++ index.js
@@ -0,0 +1,13 @@
+// const http = require("http");
+
+// const server = http.createServer((request, response) => {
+//   response.write("hello pratik ");
+//   response.end();
+// }).listen(4500);
+
+
+
+const colors = require('colors')
+// console.log("hello".red)
+
+console.log("dd".green)
